6ce732004bb1f38966849d64138b740e7c206d54
maxime <mux99@live.be>
2023-05-15 12:29:05 +0200

    fixes

--- code/main.py
+++ code/main.py
@@ -21,8 +21,8 @@ from machine import Pin
 # pin 21, GP16 -> [output] TRANSISTOR 7seg 2
 
 # LEDs ----------------------------------------------------------------------------------
-# pin 19, GP14 -> [output] green LED
-# pin 20, GP15 -> [output] red LED
+# pin , GP12 -> [output] green LED
+# pin , GP13 -> [output] red LED
 # GND
 
 # CODE ----------------------------------------------------------------------------------
@@ -37,17 +37,17 @@ data_2 = Pin(20, mode=Pin.OUT)
 data_3 = Pin(19, mode=Pin.OUT)
 data_dot = Pin(22, mode=Pin.OUT)
 
-seg_1 = Pin(17, mode=Pin.OUT)
+seg_1 = Pin(15, mode=Pin.OUT)
 seg_2 = Pin(16, mode=Pin.OUT)
 
-led_green = Pin(14, mode=Pin.OUT)
-led_red = Pin(15, mode=Pin.OUT)
+led_green = Pin(12, mode=Pin.OUT)
+led_red = Pin(13, mode=Pin.OUT)
 time.sleep_us(100)
 
 #global variables
 sensor_timeout = 30000
 distance_cm = 0
-distance_target = 0
+distance_target = 50
 
 #pins default values
 sensor_trig.value(0)
@@ -62,55 +62,34 @@ led_green.value(1)
 led_red.value(0)
 
 def itob(n):
-    out = [0,0,0,0]
-    if n >= 8:
-        out[3] = 1
-        n -= 8
-    if n >= 4:
-        out[2] = 1
-        n -= 4
-    if n >= 2:
-        out[1] = 1
-        n -= 2
-    if n >= 1:
+    if n == 0:
+        return "00000000"
+    tmp = str(int(n))+"0"
+    out = [0,0,0,0,0,0,0,0]
+    print(tmp)
+    if int(tmp[0]) > 8:
         out[0] = 1
-        n -= 1
+    if int(tmp[0]) > 4:
+        out[1] = 1
+    if int(tmp[0]) > 2:
+        out[2] = 1
+    if int(tmp[0]) > 1:
+        out[3] = 1
+        
+    if int(tmp[1]) > 8:
+        out[4] = 1
+    if int(tmp[1]) > 4:
+        out[5] = 1
+    if int(tmp[1]) > 2:
+        out[6] = 1
+    if int(tmp[1]) > 1:
+        out[7] = 1
+        
     return out
-
-
-def display_thread(distance_cm):
-    while True:
-        d = str(round(distance_cm))
-        if distance_cm >= 100:
-            data_dot.value(1)
-        else:
-            data_dot.value(0)
-        if distance_cm <= distance_target:
-            led_red.value(1)
-            led_green.value(0)
-        else:
-            led_green.value(1)
-        seg_1.value(1)
-        seg_2.value(0)
-        tmp = itob(int(d[0]))
-        data_3.value(tmp[3])
-        data_2.value(tmp[2])
-        data_1.value(tmp[1])
-        data_0.value(tmp[0])
-        led_red.value(0)
-        seg_1.value(0)
-        seg_2.value(1)
-        if (len(d) > 1):
-            tmp = itob(int(d[1]))
-            data_3.value(tmp[3])
-            data_2.value(tmp[2])
-            data_1.value(tmp[1])
-            data_0.value(tmp[0])
-
 
 # MAIN
 uart = machine.UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
-_thread.start_new_thread(display_thread, (distance_cm, ))
+#_thread.start_new_thread(display_thread, (distance_cm, ))
 while True:
     # read usb
     if uart.any(): 
@@ -137,12 +116,36 @@ while True:
     try:
         pulse_len = (machine.time_pulse_us(sensor_echo, 1, sensor_timeout))
         distance_cm = (pulse_len / 2) / 29.1
-        print(distance_cm)
-        time.sleep_us(100000)
+        time.sleep_us(15000)
 
     except:
         #allow timeout to happen whitout breaking everything (could be more precise tho)
 
         #display 0 if the sensor is too far from any objects
         distance_cm = 0
+        
+    #display led
+    if distance_cm > distance_target:
+        led_green.value(0)
+        led_red.value(1)
+    else:
+        led_green.value(1)
+        led_red.value(0)
+        
+    #display 7-seg
+    tmp = itob(distance_cm)
+    print(tmp)
+    seg_2.value(0)
+    seg_1.value(1)
+    data_0.value(tmp[4])
+    data_1.value(tmp[5])
+    data_2.value(tmp[6])
+    data_3.value(tmp[7])
+    time.sleep_us(20000)
+    seg_1.value(0)
+    seg_2.value(1)
+    data_0.value(tmp[1])
+    data_1.value(tmp[2])
+    data_2.value(tmp[3])
+    data_3.value(tmp[4])
         
